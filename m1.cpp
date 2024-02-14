66595ff436cc91ceef0b190b13493e4264c02a94
Jasmi Vekariya <jasmivekariya1@gmail.com>
2024-02-13 20:00:27 -0500

    Adding logic to print the characters and their mean values

--- m1.cpp
+++ m1.cpp
@@ -1,1 +1,30 @@
 #include <stdio.h>
+int main()
+{
+	//Declaring and initializing variable.
+	// declaring the initial of my last name that is 'v'
+	const char initial = 'v';
+	int i = 'a', addition = 0, count = 0, mean = 0;
+
+	//Using loop to print alphabets and the ASCII in front of the letter 'v'.
+	//the loop checks the letter is less than or equal to 'z' 
+	while (i <= 'z')
+	{
+		addition += i;
+		count++;
+
+		if (i == initial)
+			//If the letter is the initial the ASCII value will be displayed
+		{
+			printf("%c\t%d\n", i, mean);
+		}
+		else
+			//mean of ASCII values other characters will be displayed.
+		{
+			mean = addition / count;
+			printf("%c\t%c\n", i, mean);
+		}
+		i++;
+	}
+	return 0;
+}
